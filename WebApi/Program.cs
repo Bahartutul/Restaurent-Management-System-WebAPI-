4798169583eb06ea0f9a7a2a939828e6e95a1366
bahartutul <tutulaiub@gmail.com>
2019-11-27 13:29:30 +0600

    WebApi

---
+++ WebApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestaurentMgt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WebApi/RestaurentApi/Controllers/OrdersController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RestaurentApi.Models;
+
+namespace RestaurentApi.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        private ResContext db = new ResContext();
+
+        // GET: api/Orders
+        public IQueryable<Order> GetOrders()
+        {
+            return db.Orders;
+        }
+
+        // GET: api/Orders/5
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult GetOrder(int id)
+        {
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(order);
+        }
+
+        // PUT: api/Orders/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutOrder(int id, Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != order.OrderId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Orders
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult PostOrder(Order order)
+        {
+            try
+            {
+                db.Orders.Add(order);
+                foreach(var row in order.OrderItems)
+                {
+                    db.OrderItems.Add(row);
+                }
+                db.SaveChanges();
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        // DELETE: api/Orders/5
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult DeleteOrder(int id)
+        {
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            db.Orders.Remove(order);
+            db.SaveChanges();
+
+            return Ok(order);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrderExists(int id)
+        {
+            return db.Orders.Count(e => e.OrderId == id) > 0;
+        }
+    }
+}
